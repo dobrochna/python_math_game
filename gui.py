1667aa56a3a2443af8800fdfcc2b0b34c9cc89af
dobrochna <dobrochna.niezgoda@gmail.com>
2018-12-11 18:27:47 +0100

    displaying grid

--- gui.py
+++ gui.py
@@ -6,13 +6,42 @@ class Window:
         self.root = tk.Tk()
         self.root.geometry("500x500")
         self.image = tk.PhotoImage(file="logo.png")
-        self.logo = tk.Label(self.root, image=self.image, padx=10, pady=10).grid(row=0, column=7, columnspan=3, rowspan=3)
-        self.label = tk.Label(self.root, text='Simple Puzzle game', padx=100, pady=10, fg="dark blue", font="Helvetica 12 bold").grid(row=0, column=0, columnspan=6)
-        self.msg = tk.Label(self.root, text='Choose the board size for the game', padx=10, pady=10, font="Helvetica 10").grid(row=5, column=3, columnspan=5)
-        self.display_buttons()
+        self.logo = tk.Label(self.root, image=self.image, padx=10, pady=10).place(x=10, y=10)
+        self.label = tk.Label(self.root, text='Simple Puzzle game', padx=10, pady=10, fg="dark blue",
+                              font="Helvetica 12 bold").place(x=80, y=20)
+        self.msg = tk.Label(self.root, text='Choose the board size for the game', padx=10, pady=10,
+                            font="Helvetica 10")
+        self.msg.place(x=130, y=100)
+        self.init_buttons()
 
-    def display_buttons(self):
-        three_button = tk.Button(self.root, text="3x3", activebackground="light blue", bd=4).grid(row=6, column=3)
-        four_button = tk.Button(self.root, text="4x4", activebackground="light blue", bd=4).grid(row=6, column=4)
-        five_button = tk.Button(self.root, text="5x5", activebackground="light blue", bd=4).grid(row=6, column=5)
-        six_button = tk.Button(self.root, text="6x6", activebackground="light blue", bd=4).grid(row=6, column=6)
+    def init_buttons(self):
+        self.three_button = tk.Button(self.root, text="3x3", activebackground="light blue", bd=4,
+                                      command=lambda: self.display_table(3))
+        self.three_button.place(x=150, y=150)
+        self.four_button = tk.Button(self.root, text="4x4", activebackground="light blue", bd=4,
+                                     command=lambda: self.display_table(4))
+        self.four_button.place(x=200, y=150)
+        self.five_button = tk.Button(self.root, text="5x5", activebackground="light blue", bd=4,
+                                     command=lambda: self.display_table(5))
+        self.five_button.place(x=250, y=150)
+        self.six_button = tk.Button(self.root, text="6x6", activebackground="light blue", bd=4,
+                                    command=lambda: self.display_table(6))
+        self.six_button.place(x=300, y=150)
+        self.quit_button = tk.Button(self.root, text="quit", activebackground="red", bd=4,
+                                     command=self.root.destroy).place(x=450, y=450)
+
+    def display_table(self, size):
+        self.destroy_buttons()
+        self.canvas = tk.Canvas()
+
+        for number in range(0, size+1):
+            for value in range(0, size+1):
+                self.canvas.create_rectangle(0+value*40, 0+number*40, 40+value*40, 40+number*40)
+        self.canvas.place(x=180-size*10, y=180-size*10)
+
+    def destroy_buttons(self):
+        self.msg.destroy()
+        self.three_button.destroy()
+        self.four_button.destroy()
+        self.five_button.destroy()
+        self.six_button.destroy()
--- main_game.py
+++ main_game.py
@@ -3,22 +3,7 @@ import gui
 
 
 if __name__ == "__main__":
-    size = 0
-
-    print("Hello in the game!\nHow big table do you want?")
-
-    while True:
-        try:
-            size = int(input("Pass:\n-'3' for table 3x3\n-'4' for table 4x4\n-'5' for table 5x5\n-'6' for table 6x6\n"))
-        except ValueError:
-            print('size must be an integer')
-        else:
-            if size not in range(3, 7):
-                print('Sorry max size is 6x6 and min size is 3x3, try one more time!')
-            else:
-                print('Great, lets play!\nGood luck! Here is your table:')
-                break
-
+    size = 4
     game_table = table.Table(size)
     window = gui.Window()
     window.root.mainloop()
