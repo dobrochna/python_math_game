826ce4d44cd035049925c7fe87b846013c88ba7e
dobrochna <dobrochna.niezgoda@gmail.com>
2018-12-18 21:34:51 +0100

    I actually now why its not working...

--- gui.py
+++ gui.py
@@ -15,30 +15,55 @@ class Window:
                             font="Helvetica 10")
         self.msg.place(x=130, y=100)
         self.init_buttons()
+        self.table_buttons = []
 
     def init_buttons(self):
         # init buttons in window to choose the size of the table
         self.three_button = tk.Button(self.root, text="3x3", activebackground="#0099e6", bd=4,
-                                      command=lambda: self.init_table_size(3))
+                                      command=lambda: self.init_grid(3))
         self.three_button.place(x=150, y=150)
         self.four_button = tk.Button(self.root, text="4x4", activebackground="#0099e6", bd=4,
-                                     command=lambda: self.init_table_size(4))
+                                     command=lambda: self.init_grid(4))
         self.four_button.place(x=200, y=150)
         self.five_button = tk.Button(self.root, text="5x5", activebackground="#0099e6", bd=4,
-                                     command=lambda: self.init_table_size(5))
+                                     command=lambda: self.init_grid(5))
         self.five_button.place(x=250, y=150)
         self.six_button = tk.Button(self.root, text="6x6", activebackground="#0099e6", bd=4,
-                                    command=lambda: self.init_table_size(6))
+                                    command=lambda: self.init_grid(6))
         self.six_button.place(x=300, y=150)
         self.quit_button = tk.Button(self.root, text="quit", activebackground="#ff8080", bd=4,
                                      command=self.root.destroy).place(x=450, y=450)
 
-    def init_table_size(self, size):
-        self.size = size
+    def init_grid(self, size):
+        # first destroy buttons
         self.destroy_buttons()
-        self.lets_start = tk.Label(self.root, text="You selected a board: %ix%i. Great, let's play!" % (size, size),
-                                   padx=10, pady=10, font="Helvetica 10").place(x=100, y=100)
-        self.table_grid = Grid(self.root, self.size)
+
+        # then create grid of buttons for the game
+        game_table = table.Table(size)
+
+        table_buttons_sublist = []
+        for row in range(0, size):
+            for column in range(0, size):
+                table_buttons_sublist.append(tk.Button(self.root, text=str(game_table.num_table[row][column]),
+                                                       height=2, width=4).
+                                                       place(x=250+(-(size/2)+column)*40, y=250+(-(size/2)+row)*40))
+            self.table_buttons.append(table_buttons_sublist)
+            table_buttons_sublist.clear()
+
+        for row in range(0, size):
+            print(row)
+            for column in range(0, size):
+                print(column)
+                self.table_buttons[row][column].configure(command=lambda: self.button_clicked(row, column))
+
+        # then print sums values
+        self.sums_rows = [tk.Label(self.root, text=str(game_table.sums_rows[row]), height=2, width=4).
+                          place(x=250+(-(size/2)+size)*40, y=250+(-(size/2)+row)*40) for row in range(0, size)]
+        self.sums_columns = [tk.Label(self.root, text=str(game_table.sums_columns[column]),  height=2, width=4).
+                             place(x=250+(-(size/2)+column)*40, y=250+(-(size/2)+size)*40) for column in range(0, size)]
+
+    def button_clicked(self, row, column):
+        self.table_buttons[row][column].configure(bg="red")
 
     def destroy_buttons(self):
         self.msg.destroy()
@@ -46,46 +71,3 @@ class Window:
         self.four_button.destroy()
         self.five_button.destroy()
         self.six_button.destroy()
-
-
-class Grid():
-    def __init__(self, master, size):
-        self.size = size
-        self.canvas = tk.Canvas(master, width=260, height=260)
-        self.canvas.place(x=120, y=150)
-        self.grid_step = 35
-        self.start_x = 120-(self.size/2)*self.grid_step
-        self.start_y = 120-(self.size/2)*self.grid_step
-
-        # init game table
-        game_table = table.Table(self.size)
-
-        # print table with numbers
-        for row in range(0, self.size):
-            for column in range(0, self.size):
-                self.canvas.create_rectangle(self.start_x+column*self.grid_step, self.start_y+row*self.grid_step,
-                                             self.start_x+(column+1)*self.grid_step, self.start_y+(row+1)*self.grid_step,
-                                             fill="#b3d1ff")
-                self.canvas.create_text(self.start_x+18+column*self.grid_step, self.start_y+18+row*self.grid_step,
-                                        text=str(game_table.num_table[row][column]), font="Helvetica 10 bold")
-
-        # print rows sums
-        for row in range(0, self.size):
-            self.canvas.create_text(self.start_x+18+self.size*self.grid_step, self.start_y+18+row*self.grid_step,
-                                    text=str(game_table.sums_rows[row]), font="Helvetica 10 bold")
-
-        # print columns sums
-        for column in range(0, self.size):
-            self.canvas.create_text(self.start_x+18+column*self.grid_step, self.start_y+18+self.size*self.grid_step,
-                                    text=str(game_table.sums_columns[column]), font="Helvetica 10 bold")
-
-    def bind_mouse_click(self, callback=None):
-        if callback is None:
-            callback = self.change_colour_on_click
-        self.canvas.bind("<Button-1>", callback)
-
-    def change_colour_on_click(self, event, color='green'):
-        pass
-
-
-
